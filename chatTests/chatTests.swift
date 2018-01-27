02e44f2dc17f0a6e8c82746ba3f449bbb12a5a63
dsean <dsean@kimo.com>
2018-01-27 17:04:35 +0800

    remove test account

--- chatTests/chatTests.swift
+++ chatTests/chatTests.swift
@@ -11,7 +11,6 @@ import XCTest
 
 class chatTests: XCTestCase {
     
-    var detailViewController: LogInViewController!
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
